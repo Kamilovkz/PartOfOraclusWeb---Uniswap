e3e1b2f2d9d7614b5611ca6abd58abfc7d815832
unknown <001kamilov@gmail.com>
2022-06-05 22:57:52 +0600

    First changes

---
+++ app.js
@@ -0,0 +1,22 @@
+const Web3 = require('web3')
+const web3 = new Web3('https://mainnet.infura.io/v3/4ab138954e4e4b419660fa9a4c5e7e17')
+
+const abi = [{"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":false,"internalType":"address","name":"pair","type":"address"},{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"PairCreated","type":"event"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allPairs","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"allPairsLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"}],"name":"createPair","outputs":[{"internalType":"address","name":"pair","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"feeTo","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"feeToSetter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeTo","type":"address"}],"name":"setFeeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"name":"setFeeToSetter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+const address = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+
+const contract = new web3.eth.Contract(abi, address)
+
+
+contract.getPastEvents(
+    'allEvents', 
+    { 
+        fromBlock: 14780256, 
+        toBlock: 'latest'
+    }, 
+    (err, events) => { console.log(events) }
+  )
+// contract.getPastEvents('allEvents', {
+//     filter: {myIndexedParam: [20,23]}, // Using an array means OR: e.g. 20 or 23
+//     fromBlock: 0,
+//     toBlock: 'latest'
+// }, function(err, events){ console.log(events); })
